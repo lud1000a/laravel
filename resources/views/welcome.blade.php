b048be62e6ca09fe4a684eee8e2c6571b02d7c05
Lud1000a <oliveiraludmila196@gmail.com>
2023-09-04 14:55:27 -0300

    Puxando do BD

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,18 +7,4 @@
 @foreach($events as $event)
     <p>{{ $event->title}} -- {{$event -> description}}</p>
 @endforeach
-<body class="antialiased">
-        @if(1>-1)
-        <p>comparação do if</p>
-        @endif
-        <p>para imprimir a {{ $var }}</p>
-        <hr>
-        @foreach($array as $value)
-        <p>{{ $value }}</p>
-        @endforeach
-        <p>{{ count($array) }}</p>
-        <br>
-        <br>
-    </body>
-</html>
 @endsection
